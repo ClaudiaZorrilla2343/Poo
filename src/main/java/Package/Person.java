d2fd6ae2305c4bf63632879ef223fb45a3097de1
ClaudiaZorrilla2343 <claudiaa2343@gmail.com>
2022-03-18 21:57:48 -0500

    Creación de la clase persona con sus atributos y el metodo Nombre

---
+++ src/main/java/Package/Person.java
@@ -0,0 +1,22 @@
+package Package;
+
+import java.util.Date;
+
+public class Person {
+
+    String name;
+    String lastName1;
+    String lastName2;
+    Date dateBirth;
+    float height;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
+
