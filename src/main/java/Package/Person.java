a26e42ffec88e772da7136cb490356c4d15f8ba4
ClaudiaZorrilla2343 <claudiaa2343@gmail.com>
2022-03-19 17:22:06 -0500

    Documentación de la clase Persona

--- src/main/java/Package/Person.java
+++ src/main/java/Package/Person.java
@@ -2,6 +2,11 @@ package Package;
 
 import java.util.Date;
 
+/**
+ *Esta clase es del objeto persona
+ *
+ * @autor Claudia Zorrilla Rojas
+ */
 public class Person {
 
     String name;
@@ -10,10 +15,20 @@ public class Person {
     Date dateBirth;
     float height;
 
+    /**
+     * Este método me muestra el atributo nombre de la clase persona
+     *
+     * @return Valor del atributo nombre
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Este método me ayuda a modificar el valor del atributo nombre de la clase persona
+     *
+     * @param name atributo a modificar
+     */
     public void setName(String name) {
         this.name = name;
     }
