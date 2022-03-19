ae54ef4a7dc3210234dc0f1dee99b8799171ee53
ClaudiaZorrilla2343 <claudiaa2343@gmail.com>
2022-03-19 17:53:01 -0500

    Documentación de la clase Fruta

--- src/main/java/Package/Fruit.java
+++ src/main/java/Package/Fruit.java
@@ -1,17 +1,31 @@
 package Package;
 
 import java.util.ArrayList;
-
+/**
+ *Esta clase es del objeto Fruta
+ *
+ * @autor Claudia Zorrilla Rojas
+ */
 public class Fruit {
 
     public String name;
     private float averageWeihgt;
     public ArrayList<String> colors;
 
+    /**
+     * Este método muestra el atributo colores de la clase Fruta
+     * @return Valor del atributo colores
+     */
     public ArrayList<String> getColors() {
         return colors;
     }
 
+    /**
+     * Este método me ayuda a modificar el valor del atributo colores
+     * de la clase Fruta
+     *
+     * @param colors atributo a modificar
+     */
     public void setColors(ArrayList<String> colors) {
         this.colors = colors;
     }
