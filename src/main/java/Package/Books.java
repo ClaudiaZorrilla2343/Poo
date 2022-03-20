4ebde3dca2ec0e2d8afb9d956b07e41d0e87e134
ClaudiaZorrilla2343 <claudiaa2343@gmail.com>
2022-03-20 14:12:27 -0500

    Se agrega los metodos y documentación de la clase Libros

--- src/main/java/Package/Books.java
+++ src/main/java/Package/Books.java
@@ -1,5 +1,10 @@
 package Package;
 
+/**
+ * Esta clase es del objeto Libros
+ *
+ * @autor Claudia Zorrilla Rojas.
+ */
 public class Books {
 
     public String category;
@@ -9,7 +14,40 @@ public class Books {
     public  String component;
     public int yearPublication;
 
+    /**
+     * Constructor con el atributo category asignando valor
+     */
+    public Books(){
+        this.category = "Comedia";
+    }
+
+    /**
+     * Este método hace posible la modificación del valor del atributo
+     * categoria de las clase Libros.
+     *
+     * @param category
+     */
+    public void setCategory(String category){
+        this.category = category;
+    }
+
+    /**
+     * Este método muestra el atributo categoria de la clase Libros
+     * @return el valor del atributo categoria.
+     */
+    public String getCategory(){
+        return category;
+    }
 
 
+    public static void main(String[] args) {
+
+        /**
+         * Ensayo
+         */
+        Books gender = new Books();
+        String  knowCategory = gender.getCategory();
+        System.out.println(knowCategory);
+    }
 
 }
